e7e9ceccb2395c0cab265e5663f4f1008b4c9de4
iortega <inigo@iortega.xyz>
2021-02-02 13:45:09 +0100

    FIX

--- convert_pcap_csv.sh
+++ convert_pcap_csv.sh
@@ -60,11 +60,11 @@ convert() {
 	fi
 
 	filename=$(ls -t "${output_dir}" | head -n1)
-	filepath="${output_dir}/${filename}"
+	filepath="${output_dir}${filename}"
 	names=$(head -n1 "${filepath}")
 	sed -i "/Flow ID/d" "${filepath}"
 	echo "$names" | cat - "${filepath}" > "/tmp/${filename}"
-	mv "/tmp/${output_dir}" "${filepath}"
+	mv "/tmp/${filename}" "${filepath}"
 
 
 	## Rearrange the PCAP due to other CICFlowMeter versions' compatibility
